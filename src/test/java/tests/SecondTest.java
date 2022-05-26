d077301cd61ecca3c2d1d0c5916405a150e864e1
Md. Imran <mohammad.imran@dsinnovators.com>
2022-05-26 12:25:19 +0600

    CICD Commit

--- src/test/java/tests/SecondTest.java
+++ src/test/java/tests/SecondTest.java
@@ -21,7 +21,7 @@ public class SecondTest extends BaseTest{
         DriverManager.getDriver().manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
 
         WebElement inputBox = DriverManager.getDriver().findElement(By.xpath(FrameworkConstants.buyEmptyText()));
-        //DriverManager.getDriver().findElement(By.xpath("//body/main[1]/article[1]/section[3]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[3]/input[1]")).sendKeys("100");
+
         String textInsideInputBox = inputBox.getAttribute("value");
         System.out.println("value print : " + textInsideInputBox);
         // Check whether input field is blank
